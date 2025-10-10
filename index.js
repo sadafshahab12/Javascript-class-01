0a5b18ea0cb9c1945ae452bbbf17cd172277ec39
sadafshahab12 <sadafshahab123@yahoo.com>
2025-10-10 20:15:11 +0500

    class 01 done

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-// let myHeader = document.getElementById("myHeader");
-// let changeBtn= document.getElementById("changeBtn");
-// console.log(myHeader)
-// function changeText() {
-//   myHeader.innerHTML = "Today is second class";
-//   console.log("Your text is changed")
-// }
-// changeBtn.addEventListener("click", changeText)
+let myHeader = document.getElementById("myHeader");
+let changeBtn = document.getElementById("changeBtn");
+console.log(myHeader);
+function changeText() {
+  myHeader.innerHTML = "Today is second class";
+  console.log("Your text is changed");
+}
+changeBtn.addEventListener("click", changeText);
 
-var name = "sadaf"; // global scope
-var name = "sadaf shahab";
+var name = "sadaf"; // global scope variable
+var name = "sadaf shahab"; // global scope variable
 console.log(name);
 
-// function names() {
-//   var student = "Resham";
-//   console.log(student);
-// }
-// names();
-// console.log(student)
-{
-    var student = "Resham Khan 123";
-}
-console.log(student)
 function names() {
-    console.log(student)
+  var student = "Resham"; //function scope variable (can be accessed only inside the function)
+  console.log(student);
 }
-names()
+names();
+console.log(student);
+
+//block scope {}
+{
+  var student = "Resham Khan 123"; //global scope variable (can be accessed outside the block)
+}
+
+console.log(student);
+function names() {
+  console.log(student);
+}
+names();
