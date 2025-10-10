4d26384e1f761cc3a4e3ba1b1f2a091c22028492
sadafshahab12 <sadafshahab123@yahoo.com>
2025-10-10 20:13:35 +0500

    class 01 done

---
+++ index.js
@@ -0,0 +1,27 @@
+// let myHeader = document.getElementById("myHeader");
+// let changeBtn= document.getElementById("changeBtn");
+// console.log(myHeader)
+// function changeText() {
+//   myHeader.innerHTML = "Today is second class";
+//   console.log("Your text is changed")
+// }
+// changeBtn.addEventListener("click", changeText)
+
+var name = "sadaf"; // global scope
+var name = "sadaf shahab";
+console.log(name);
+
+// function names() {
+//   var student = "Resham";
+//   console.log(student);
+// }
+// names();
+// console.log(student)
+{
+    var student = "Resham Khan 123";
+}
+console.log(student)
+function names() {
+    console.log(student)
+}
+names()
